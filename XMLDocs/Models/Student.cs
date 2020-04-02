ad51ed88d16c6fe34e68b7e8cad41ff888cd069e
Pranav <pranavs970@gmail.com>
2020-04-01 23:58:54 -0400

    First commit

---
+++ XMLDocs/Models/Student.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace XMLDocs.Models
+{
+    public class Student
+    {
+         public static string HeaderRow = $"{nameof(Student.StudentId)},{nameof(Student.FirstName)},{nameof(Student.LastName)},{nameof(Student.DateOfBirth)},{nameof(Student.ImageData)}";
+
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _DateOfBirth;
+        public string DateOfBirth
+        {
+            get { return _DateOfBirth; }
+            set
+            {
+                _DateOfBirth = value;
+
+                //Convert DateOfBirth to DateTime
+                DateTime dtOut;
+                DateTime.TryParse(_DateOfBirth, out dtOut);
+                DateOfBirthDT = dtOut;
+            }
+        }
+
+        public DateTime DateOfBirthDT { get; set; }
+        public string ImageData { get; set; }
+        public string MyRecord { get; set; }
+        public string AbsoluteUrl { get; set; }
+        public string Directory { get; set; }
+      //public string InfoCSVPath { get { return (Constants.FTP.BaseUrl + "/" + Directory + "/" + Constants.Locations.InfoFile); } }
+      //public string MyImagePath { get { return (Constants.FTP.BaseUrl + "/" + Directory + "/" + Constants.Locations.ImageFile); } }
+
+        public string FullPathUrl
+        {
+            get
+            {
+                return AbsoluteUrl + "/" + Directory;
+            }
+        }
+
+        public List<string> Exceptions { get; set; } = new List<string>();
+        
+        public void FromCSV(string csvdata)
+        {
+            string[] data = csvdata.Split(",", StringSplitOptions.None);
+            try
+            {
+                StudentId = data[0];
+                FirstName = data[1];
+                LastName = data[2];
+                DateOfBirth = data[3];
+                ImageData = data[4];
+            }
+            catch (Exception e)
+            {
+                Exceptions.Add(e.Message);
+            }
+        }
+
+        public bool IsValidImageFile(string imageFile)
+        {
+            try
+            {
+                // the using is important to avoid stressing the garbage collector
+                using (var test = System.Drawing.Image.FromFile(imageFile))
+                {
+                    // image has loaded and so is fine
+                    return true;
+                }
+            }
+            catch
+            {
+                // technically some exceptions may not indicate a corrupt image, but this is unlikely to be an issue
+                return false;
+            }
+        }
+
+        public void FromDirectory(string directory)
+        {
+            Directory = directory;
+
+            if (String.IsNullOrEmpty(directory.Trim()))
+            {
+                return;
+            }
+
+            string[] data = directory.Trim().Split(" ", StringSplitOptions.None);
+
+            StudentId = data[0];
+            FirstName = data[1];
+            LastName = data[2];
+        }
+
+
+
+        public string ToCSV()
+        {
+            string result = $"{StudentId},{FirstName},{LastName},{DateOfBirthDT.ToShortDateString()},{ImageData}";
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{StudentId} {FirstName} {LastName}";
+            return result;
+        }
+
+        public virtual int age
+        {
+            get
+            {
+                DateTime Now = DateTime.Now;
+                int Years = new DateTime(DateTime.Now.Subtract(DateOfBirthDT).Ticks).Year - 1;
+                DateTime PastYearDate = DateOfBirthDT.AddYears(Years);
+                int Months = 0;
+                for (int i = 1; i <= 12; i++)
+                {
+                    if (PastYearDate.AddMonths(i) == Now)
+                    {
+                        Months = i;
+                        break;
+                    }
+                    else if (PastYearDate.AddMonths(i) >= Now)
+                    {
+                        Months = i - 1;
+                        break;
+                    }
+                }
+                int Days = Now.Subtract(PastYearDate.AddMonths(Months)).Days;
+                int Hours = Now.Subtract(PastYearDate).Hours;
+                int Minutes = Now.Subtract(PastYearDate).Minutes;
+                int Seconds = Now.Subtract(PastYearDate).Seconds;
+                return Years;
+            }
+        }
+
+
+
+
+
+
+        //public int age
+        //{
+        //    get
+        //    {
+        //        return (DateTime.Now - this.DateOfBirthDT).Days / 365;
+        //    }
+        //}
+
+        //public DateTime DateOfBirthDT { get; set; }
+        //public int Age
+        //{
+        //    get
+        //    {
+        //        //DateTime now = DateTime.Now;
+        //        //int age = now.Year - DateOfBirth.Year;
+        //        //if (now < DateOfBirth.AddYears(age)) age--;
+        //        //return age;
+
+        //        DateTime today = DateTime.Today;
+        //        //DateTime bday = Convert.ToDateTime(student.DateOfBirthDT);
+        //        int age = today.Year - DateOfBirthDT.Year;
+        //        if (bday > today.AddYears(-age)) age--;
+        //        return age;
+        //    }
+        //{
+
+        //       // Console.WriteLine("Average age", )
+        //}
+
+    }
+}
---
+++ XMLDocs/Program.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using XMLDocs.Models.Utilities;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text;
+using XMLDocs.Models;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Drawing;
+using System.Drawing.Imaging;
+using DocumentFormat.OpenXml.ExtendedProperties;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+using System.Data.OleDb;
+
+
+namespace XMLDocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student myrecord = new Student { StudentId = "200429019", FirstName = "Pranav", LastName = "Sharma" };
+
+            List<Student> students = new List<Student>();
+
+            List<string> directories = FTP.GetDirectory(Constants.FTP.BaseUrl);
+
+
+            foreach (var directory in directories)
+            {
+                Student student = new Student() { AbsoluteUrl = Constants.FTP.BaseUrl };
+                student.FromDirectory(directory);
+
+                Console.WriteLine(student);
+                string infoFilePath = student.FullPathUrl + "/" + Constants.Locations.InfoFile;
+
+                bool fileExists = FTP.FileExists(infoFilePath);
+                if (fileExists == true)
+                {
+                    string csvPath = $@"/Users/pranavsharma/Desktop/Student Data/{directory}.csv";
+
+                    // FTP.DownloadFile(infoFilePath, csvPath);
+                    byte[] bytes = FTP.DownloadFileBytes(infoFilePath);
+                    string csvData = Encoding.Default.GetString(bytes);
+
+                    string[] csvlines = csvData.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+                    if (csvlines.Length != 2)
+                    {
+                        Console.WriteLine("Error in CSV format");
+                    }
+                    else
+                    {
+                        student.FromCSV(csvlines[1]);
+                    }
+
+                    Console.WriteLine("Found info file:");
+                    student.MyRecord = "yes";
+                }
+                else
+                {
+                    Console.WriteLine("Could not find info file:");
+                    student.MyRecord = "No";
+                }
+
+                Console.WriteLine("\t" + infoFilePath);
+
+                string imageFilePath = student.FullPathUrl + "/" + Constants.Locations.ImageFile;
+                bool imageFileExists = FTP.FileExists(imageFilePath);
+                //try
+                //{
+                    //var imageBytes = FTP.DownloadFileBytes(imageFilePath);
+                    //Image myimage = Imaging.ByteArrayToImage(imageBytes);
+                    //myimage.Save($@"/Users/pranavsharma/Desktop/student/{student.StudentId}.jpg");
+                    //string base64 = Imaging.ImageToBase64(myimage, ImageFormat.Jpeg);
+
+                    if (imageFileExists == true)
+                    {
+                        Console.WriteLine("Found image file : ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not find image file:");
+                    }
+                    
+                    
+                //}
+                //catch (Exception )
+                //{
+                   
+                //}
+
+                Console.WriteLine("\t" + imageFilePath);
+
+            students.Add(student);
+                //Console.WriteLine(directory);
+                Console.WriteLine(" \t Count of student : {0}", students.Count);
+                Console.WriteLine("  \t Age of Student : {0} ", student.age);
+                Console.WriteLine("  \t Student Record : {0} ", student.MyRecord);
+
+            }
+
+            Student me = students.SingleOrDefault(x => x.StudentId == myrecord.StudentId);
+            Student meUsingFind = students.Find(x => x.StudentId == myrecord.StudentId);
+
+            var avgage = students.Average(x => x.age);
+            var minage = students.Min(x => x.age);
+            var maxage = students.Max(x => x.age);
+
+
+            Console.WriteLine("  \n\t Name Searched With Query: {0} ", meUsingFind);
+            Console.WriteLine("  \t Average of Student age is: {0} ", avgage);
+            Console.WriteLine("  \t Minimum of Student age is: {0} ", minage);
+            Console.WriteLine("  \t Maximum of Student age is: {0} ", maxage);
+
+            //save to csv
+            string studentsCSVPath = $"{Constants.Locations.DataFolder}//students.csv";
+            //Establish a file stream to collect data from the response
+            using (StreamWriter fs = new StreamWriter(studentsCSVPath))
+            {
+                foreach (var student in students)
+                {
+                    fs.WriteLine(student.ToCSV());
+                }
+            }
+
+            string studentsWordPath = $"{Constants.Locations.DataFolder}//students.docx";
+
+            string studentsImagePath = $"{Constants.Locations.ImagesFolder}//myimage.jpg";
+
+            using (WordprocessingDocument wordDocument =
+                WordprocessingDocument.Create(studentsWordPath, WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+
+                Paragraph para = body.AppendChild(new Paragraph());
+
+                Run run = para.AppendChild(new Run());
+
+                foreach (var student in students)
+                {
+                    run.AppendChild(new Text("My name :  "));
+                    run.AppendChild(new Text(student.FirstName.ToString()));
+                    run.AppendChild(new Text("  ,  "));
+                    run.AppendChild(new Text("My Student id : "));
+                    run.AppendChild(new Text(student.StudentId.ToString()));
+                    run.AppendChild(new Text("  ,  "));
+                    using (FileStream stream = new FileStream(studentsImagePath, FileMode.Open))
+                    {
+                        imagePart.FeedData(stream);
+                    }
+
+                    AddImageToBody(wordDocument, mainPart.GetIdOfPart(imagePart));
+
+                    run.AppendChild(new Break() { Type = BreakValues.Page });
+
+                }
+            }
+
+            // Create Excel sheet and fetch data from FTP
+            string studentsExcelPath = $"{Constants.Locations.DataFolder}//students.xlsx";
+
+            using SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+            Create(studentsExcelPath, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet(new DocumentFormat.OpenXml.Spreadsheet.SheetData());
+
+            // Add Sheets to the Workbook.
+            DocumentFormat.OpenXml.Spreadsheet.Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild<DocumentFormat.OpenXml.Spreadsheet.Sheets>(new DocumentFormat.OpenXml.Spreadsheet.Sheets());
+
+            // Append a new worksheet and associate it with the workbook.
+            DocumentFormat.OpenXml.Spreadsheet.Sheet sheet = new DocumentFormat.OpenXml.Spreadsheet.Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+            DocumentFormat.OpenXml.Spreadsheet.SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<DocumentFormat.OpenXml.Spreadsheet.SheetData>();
+            var excelRows = sheetData.Descendants<DocumentFormat.OpenXml.Spreadsheet.Row>().ToList();
+            //var excelcolumns = sheetData.Descendants<DocumentFormat.OpenXml.Spreadsheet.Column>().ToList();
+            int rowindex = 1;
+            //int columnindex = 1;
+            foreach (var student in students)
+            {
+                DocumentFormat.OpenXml.Spreadsheet.Row row = new DocumentFormat.OpenXml.Spreadsheet.Row();
+                DocumentFormat.OpenXml.Spreadsheet.Columns cs = new DocumentFormat.OpenXml.Spreadsheet.Columns();
+                row.RowIndex = (UInt32)rowindex;
+
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(student.FirstName.ToString())
+                };
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell1 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(student.LastName.ToString())
+                };
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell2 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(student.StudentId.ToString())
+
+                };
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell3 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(Convert.ToString(student.MyRecord.ToString()))
+                
+                };
+
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell4 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(student.age.ToString())
+                };
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell5 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(Convert.ToString(student.DateOfBirthDT.ToString()))
+                };
+                DocumentFormat.OpenXml.Spreadsheet.Cell cell6 = new DocumentFormat.OpenXml.Spreadsheet.Cell()
+                {
+                    DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String,
+                    CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(Convert.ToString(Guid.NewGuid().ToString()))
+                };
+
+                row.Append(cell);
+                row.Append(cell1);
+                row.Append(cell2);
+                row.Append(cell3);
+                row.Append(cell4);
+                row.Append(cell5);
+                row.Append(cell6);
+
+                sheetData.Append(row);
+
+                //how to write the data in cell
+                rowindex++;
+            }
+
+            sheets.Append(sheet);
+
+            workbookpart.Workbook.Save();
+
+            // Close the document.
+            spreadsheetDocument.Close();
+
+            string studentsxmlPath = $"{Constants.Locations.DataFolder}//students.xml";
+            //Establish a file stream to collect data from the response
+            using (StreamWriter fs = new StreamWriter(studentsxmlPath))
+            {
+                XmlSerializer x = new XmlSerializer(students.GetType());
+                x.Serialize(fs, students);
+                Console.WriteLine();
+            }
+
+            //4.Upload the files to My FTP
+            foreach (var student in students)
+            {
+                //FTP.UploadFile(studentsxmlPath, Constants.FTP.BaseUrl + "/200429013 Pranav Sharma/students.csv");
+                FTP.UploadFile(studentsxmlPath, Constants.FTP.BaseUrl + "/200429013 Pranav Sharma/students.xml");
+                FTP.UploadFile(studentsxmlPath, Constants.FTP.BaseUrl + "/200429019 Pranav Sharma/students.word");
+                FTP.UploadFile(studentsxmlPath, Constants.FTP.BaseUrl + "/200429019 Pranav Sharma/students.xlsx");
+
+            }
+            return;
+        }
+
+        public static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId)
+        {
+            // Define the reference of the image.
+            var element =
+                 new DocumentFormat.OpenXml.Wordprocessing.Drawing(
+                     new DW.Inline(
+                         new DW.Extent() { Cx = 990000L, Cy = 792000L },
+                         new DW.EffectExtent()
+                         {
+                             LeftEdge = 0L,
+                             TopEdge = 0L,
+                             RightEdge = 0L,
+                             BottomEdge = 0L
+                         },
+                         new DW.DocProperties()
+                         {
+                             Id = (UInt32Value)1U,
+                             Name = "Picture 1"
+                         },
+                         new DW.NonVisualGraphicFrameDrawingProperties(
+                             new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                         new A.Graphic(
+                             new A.GraphicData(
+                                 new PIC.Picture(
+                                     new PIC.NonVisualPictureProperties(
+                                         new PIC.NonVisualDrawingProperties()
+                                         {
+                                             Id = (UInt32Value)0U,
+                                             Name = "New Bitmap Image.jpg"
+                                         },
+                                         new PIC.NonVisualPictureDrawingProperties()),
+                                     new PIC.BlipFill(
+                                         new A.Blip(
+                                             new A.BlipExtensionList(
+                                                 new A.BlipExtension()
+                                                 {
+                                                     Uri =
+                                                        "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                                                 })
+                                         )
+                                         {
+                                             Embed = relationshipId,
+                                             CompressionState =
+                                             A.BlipCompressionValues.Print
+                                         },
+                                         new A.Stretch(
+                                             new A.FillRectangle())),
+                                     new PIC.ShapeProperties(
+                                         new A.Transform2D(
+                                             new A.Offset() { X = 0L, Y = 0L },
+                                             new A.Extents() { Cx = 990000L, Cy = 792000L }),
+                                         new A.PresetGeometry(
+                                             new A.AdjustValueList()
+                                         )
+                                         { Preset = A.ShapeTypeValues.Rectangle }))
+                             )
+                             { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+                     )
+                     {
+                         DistanceFromTop = (UInt32Value)0U,
+                         DistanceFromBottom = (UInt32Value)0U,
+                         DistanceFromLeft = (UInt32Value)0U,
+                         DistanceFromRight = (UInt32Value)0U,
+                         EditId = "50D07946"
+                     });
+
+            // Append the reference to body, the element should be in a Run.
+            wordDoc.MainDocumentPart.Document.Body.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Paragraph(new Run(element)));
+        }
+    }
+}
